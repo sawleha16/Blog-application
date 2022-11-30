c97d78bf89773f3fde00c79b7f6aac7ffdf81f59
Sawleha shaikh <sawlehashaikh161@gmail.com>
2022-11-30 14:36:20 +0530

    Addded new view to Blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,7 @@ class BlogsController < ApplicationController
   def index
      @blogs = Blog.all
   end
+    def show
+      @blog = Blog.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
    root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get  "home", to: "home#index"
-  get "blogs", to: "blogs#index"
+  get  "/home", to: "home#index"
+  get "/blogs", to: "blogs#index"
+  get "/blogs/:id" , to: "blogs#show"
 end
