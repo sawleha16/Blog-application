4ea80a1bdbe3a1673b35963603152213f7435596
Sawleha shaikh <sawlehashaikh161@gmail.com>
2022-11-30 12:41:32 +0530

    Add blogs list page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
    root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  "home", to: "home#index"
-  get "blogs" ,  to: "blogs#index"
+  get "blogs", to: "blogs#index"
 end
