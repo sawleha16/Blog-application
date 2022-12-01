48e6b5698d1ae2fd816e509412a7d4d544a3bea5
Sawleha shaikh <sawlehashaikh161@gmail.com>
2022-12-01 13:25:51 +0530

    Updating  Blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,43 @@ class BlogsController < ApplicationController
   def index
      @blogs = Blog.all
   end
+
     def show
       @blog = Blog.find(params[:id])
     end
-end
+
+    def new
+      @blog = Blog.new
+    end
+
+    def create
+       @blog = Blog.new(title: "Hello Winter", body: "I m ready to have all the fun")
+       if @blog.save
+         redirect_to @blog
+       else
+         render :new, status: :unprocessable_entity
+       end
+     end
+
+    def edit
+      @blog= Blog.find(params[:id])
+    end
+
+    def update
+      @blog= Blog.find(params[:id])
+      if @blog.update(blog_params)
+        redirect_to @blog
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+     def destroy
+       @blog=Blog.find(params[:id])
+       redirect_to root_path, status: :see_other
+     end
+     end
+     private
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
